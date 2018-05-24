51d47cc17836a0a440633f842211a6d22ca3b29c
mijdavis2 <mdavisinsc@gmail.com>
2018-05-24 07:52:11 -0700

    Fetch latest ubuntu ami instead of hardcode

--- Terraform/AWS Nano Node/instances/variables.tf
+++ Terraform/AWS Nano Node/instances/variables.tf
@@ -8,23 +8,6 @@ variable "key_name" {
   default     = "Nano"
 }
 
-variable "nano_ami_id" {
-  description = "AWS Linux Ubuntu 16.04 AMI Id"
-  type        = "map"
-
-  default = {
-    "eu-west-1"    = "ami-f90a4880"
-    "eu-west-2"    = "ami-f4f21593"
-    "eu-west-3"    = "ami-0e55e373"
-    "eu-central-1" = "ami-7c412f13"
-    "us-west-1"    = "ami-925144f2"
-    "us-west-2"    = "ami-4e79ed36"
-    "us-east-1"    = "ami-43a15f3e"
-    "us-east-2"    = "ami-916f59f4"
-    "ca-central-1" = "ami-ae55d2ca"
-  }
-}
-
 variable "instance_type" {
   description = "AWS Instance Size"
   default     = "t2.micro"

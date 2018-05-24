d4e5699a2f19229fe3b203ca0a5fac18217a4752
mijdavis2 <mdavisinsc@gmail.com>
2018-05-24 07:52:11 -0700

    Fetch latest ubuntu ami instead of hardcode

--- Terraform/AWS Nano Node/instances/main.tf
+++ Terraform/AWS Nano Node/instances/main.tf
@@ -3,10 +3,26 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/userdata.sh")}"
 }
 
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
+
 #Create the Nano Instance using variables
 resource "aws_instance" "nano_instance" {
   instance_type               = "${var.instance_type}"
-  ami                         = "${lookup(var.nano_ami_id, var.region)}"
+  ami                         = "${data.aws_ami.ubuntu.id}"
   subnet_id                   = "${var.nano_subnet_id}"
   key_name                    = "${var.key_name}"
   security_groups             = ["${var.nano_sg_id}"]
